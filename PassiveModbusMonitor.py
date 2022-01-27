2d9ef472a4dd21ce1e2a8fc454ef54c7dabf0234
Entropia <mristila@gmail.com>
2022-01-27 22:33:20 +0200

    All function codes and exception handling now works, pending extensive testing

--- PassiveModbusMonitor.py
+++ PassiveModbusMonitor.py
@@ -37,8 +37,41 @@ def CheckModbusFunction(rxbuffer, nextframe, quantity):
     #print("Len:", len(rxbuffer), "Nextframe:", nextframe, "Quantity:", quantity, "Byte:", rxbuffer[len(rxbuffer) - 1])
 
     try:
-        if(nextframe == 0 and len(rxbuffer) == 8):
-            # Listening for known master requests
+        if(nextframe == 0 and len(rxbuffer) == 7):
+            # Listening for known master requests (0x0F and 0x10)
+            if(rxbuffer[1] == 'f'):
+                # Write Multiple Coils
+                deviceID = rxbuffer[0]
+                address = int(rxbuffer[2], base=16) << 8
+                address += int(rxbuffer[3], base=16)
+                quantity = int(rxbuffer[4], base=16) << 8
+                quantity += int(rxbuffer[5], base=16)
+                timestamp = datetime.datetime.now()
+                print(timestamp, "Write multiple coils for device ID ", deviceID, ", Start address =", address, "Quantity =", quantity)
+                if(quantity < 8):
+                    quantity = 1
+                else:
+                    if(quantity % 8 == 0):
+                        quantity /= 8
+                    else:
+                        quantity /= 8
+                        quantity += 1
+                return (True, 15, int(quantity))
+            elif(rxbuffer[1] == '10'):
+                # Write Multiple Registers
+                deviceID = rxbuffer[0]
+                address = int(rxbuffer[2], base=16) << 8
+                address += int(rxbuffer[3], base=16)
+                quantity = int(rxbuffer[4], base=16) << 8
+                quantity += int(rxbuffer[5], base=16)
+                timestamp = datetime.datetime.now()
+                print(timestamp, "Write multiple registers for device ID ", deviceID, ", Start address =", address, "Quantity =", quantity)
+                quantity *= 2
+                return (True, 16, int(quantity))
+            else:
+                return (False, nextframe, quantity)
+        elif(nextframe == 0 and len(rxbuffer) == 8):
+            # Listening for known master requests (0x01 thru 0x06)
             if(rxbuffer[1] == '1'):
                 # Read Coils
                 deviceID = rxbuffer[0]
@@ -93,41 +126,183 @@ def CheckModbusFunction(rxbuffer, nextframe, quantity):
                 timestamp = datetime.datetime.now()
                 print(timestamp, "Read input register for device ID", deviceID, ", Start address =", address, "Quantity =", quantity)
                 return (True, 4, quantity)
+            elif(rxbuffer[1] == '5'):
+                # Write Single Coil
+                deviceID = rxbuffer[0]
+                address = Combine(rxbuffer[2], rxbuffer[3])
+                quantity = Combine(rxbuffer[4], rxbuffer[5])
+                timestamp = datetime.datetime.now()
+                print(timestamp, "Write single coil for device ID", deviceID, ", Start address =", address, "Quantity =", quantity)
+                return (True, 5, quantity)
+            elif(rxbuffer[1] == '6'):
+                # Write Single Register
+                deviceID = rxbuffer[0]
+                address = Combine(rxbuffer[2], rxbuffer[3])
+                quantity = Combine(rxbuffer[4], rxbuffer[5])
+                timestamp = datetime.datetime.now()
+                print(timestamp, "Write single register for device ID", deviceID, ", Start address =", address, "Quantity =", quantity)
+                return (True, 6, quantity)
+            else:
+                return (False, nextframe, quantity)
+
+        elif(nextframe == 1 and len(rxbuffer) == 5):
+            # Exception for function code 1 (0x01)
+            if(rxbuffer[1] == '81'):
+                timestamp = datetime.datetime.now()
+                if(rxbuffer[2] == '2'):
+                    print(timestamp, " * Illegal Data Address exception")
+                else:
+                    print(timestamp, " * Exception! Code:", rxbuffer[2])
+                return (True, 0, 0)
             else:
                 return (False, nextframe, quantity)
         elif(nextframe == 1 and len(rxbuffer) == (5 + quantity)):
-            # Expecting reply from device for function code 1
+            # Expecting reply from device for function code 1 (0x01)
             timestamp = datetime.datetime.now()
             deviceID = rxbuffer[0]
             for x in range(0, quantity):
                 data = rxbuffer[x + 3]
-                print(timestamp, "Response", (x + 1), "from device ID", deviceID, ", data =", data)
+                print(timestamp, " * Response", (x + 1), "from device ID", deviceID, ", data =", data)
             return (True, 0, 0)
+
+        elif(nextframe == 2 and len(rxbuffer) == 5):
+            # Exception for function code 2 (0x02)
+            if(rxbuffer[1] == '82'):
+                timestamp = datetime.datetime.now()
+                if(rxbuffer[2] == '2'):
+                    print(timestamp, " * Illegal Data Address exception")
+                else:
+                    print(timestamp, " * Exception! Code:", rxbuffer[2])
+                return (True, 0, 0)
+            else:
+                return (False, nextframe, quantity)
         elif(nextframe == 2 and len(rxbuffer) == (5 + quantity)):
-            # Expecting reply from device for function code 2
+            # Expecting reply from device for function code 2 (0x02)
             timestamp = datetime.datetime.now()
             deviceID = rxbuffer[0]
             for x in range(0, quantity):
                 data = rxbuffer[x + 3]
-                print(timestamp, "Response", (x + 1), "from device ID", deviceID, ", data =", data)
+                print(timestamp, " * Response", (x + 1), "from device ID", deviceID, ", data =", data)
             return (True, 0, 0)
+
+        elif(nextframe == 3 and len(rxbuffer) == 5):
+            # Exception for function code 3 (0x03)
+            if(rxbuffer[1] == '83'):
+                timestamp = datetime.datetime.now()
+                if(rxbuffer[2] == '2'):
+                    print(timestamp, " * Illegal Data Address exception")
+                else:
+                    print(timestamp, " * Exception! Code:", rxbuffer[2])
+                return (True, 0, 0)
+            else:
+                return (False, nextframe, quantity)
         elif(nextframe == 3 and len(rxbuffer) == (5 + (quantity * 2))):
-            # Expecting reply from device for function code 3
+            # Expecting reply from device for function code 3 (0x03)
             deviceID = rxbuffer[0]
             timestamp = datetime.datetime.now()
             for x in range(0, quantity):
                 data = Combine(rxbuffer[(x * 2) + 3], rxbuffer[(x * 2) + 4])
-                print(timestamp, "Response", (x + 1), "from device ID", deviceID, ", data =", data)
+                print(timestamp, " * Response", (x + 1), "from device ID", deviceID, ", data =", data)
             if(quantity == 2):
-                print(timestamp, "Float:", IntIEEE754Decode(Combine4(rxbuffer[3], rxbuffer[4], rxbuffer[5], rxbuffer[6])))
+                print(timestamp, "   * Float:", IntIEEE754Decode(Combine4(rxbuffer[3], rxbuffer[4], rxbuffer[5], rxbuffer[6])))
             return (True, 0, 0)
+
+        elif(nextframe == 4 and len(rxbuffer) == 5):
+            # Exception for function code 4 (0x04)
+            if(rxbuffer[1] == '84'):
+                timestamp = datetime.datetime.now()
+                if(rxbuffer[2] == '2'):
+                    print(timestamp, " * Illegal Data Address exception")
+                else:
+                    print(timestamp, " * Exception! Code:", rxbuffer[2])
+                return (True, 0, 0)
+            else:
+                return (False, nextframe, quantity)
         elif(nextframe == 4 and len(rxbuffer) == (5 + (quantity * 2))):
-            # Expecting reply from device for function code 4
+            # Expecting reply from device for function code 4 (0x04)
             deviceID = rxbuffer[0]
             timestamp = datetime.datetime.now()
             for x in range(0, quantity):
                 data = Combine(rxbuffer[(x * 2) + 3], rxbuffer[(x * 2) + 4])
-                print(timestamp, "Response", (x + 1), "from device ID", deviceID, ", data =", data)
+                print(timestamp, " * Response", (x + 1), "from device ID", deviceID, ", data =", data)
+            return (True, 0, 0)
+
+        elif(nextframe == 5 and len(rxbuffer) == 5):
+            # Exception for function code 5 (0x05)
+            if(rxbuffer[1] == '85'):
+                timestamp = datetime.datetime.now()
+                if(rxbuffer[2] == '2'):
+                    print(timestamp, " * Illegal Data Address exception")
+                else:
+                    print(timestamp, " * Exception! Code:", rxbuffer[2])
+                return (True, 0, 0)
+            else:
+                return (False, nextframe, quantity)
+        elif(nextframe == 5 and len(rxbuffer) == 8):
+            # Expecting reply from device for function code 5 (0x05)
+            deviceID = rxbuffer[0]
+            address = Combine(rxbuffer[2], rxbuffer[3])
+            data = Combine(rxbuffer[4], rxbuffer[5])
+            timestamp = datetime.datetime.now()
+            print(timestamp, " * Response from device ID", deviceID, " Address", address, "is now", data)
+
+        elif(nextframe == 6 and len(rxbuffer) == 5):
+            # Exception for function code 6 (0x06)
+            if(rxbuffer[1] == '86'):
+                timestamp = datetime.datetime.now()
+                if(rxbuffer[2] == '2'):
+                    print(timestamp, " * Illegal Data Address exception")
+                else:
+                    print(timestamp, " * Exception! Code:", rxbuffer[2])
+                return (True, 0, 0)
+            else:
+                return (False, nextframe, quantity)
+        elif(nextframe == 6 and len(rxbuffer) == 8):
+            # Expecting reply from device for function code 6 (0x06)
+            deviceID = rxbuffer[0]
+            address = Combine(rxbuffer[2], rxbuffer[3])
+            data = Combine(rxbuffer[4], rxbuffer[5])
+            timestamp = datetime.datetime.now()
+            print(timestamp, " * Response from device ID", deviceID, " Address", address, "is now", data)
+
+        elif(nextframe == 15 and len(rxbuffer) == (quantity + 7)):
+            # Exception for function code 15 (0x0F)
+            if(rxbuffer[quantity + 3] == '8f'):
+                timestamp = datetime.datetime.now()
+                if(rxbuffer[quantity + 4] == '2'):
+                    print(timestamp, " * Illegal Data Address exception")
+                else:
+                    print(timestamp, " * Exception! Code:", rxbuffer[quantity + 4])
+                return (True, 0, 0)
+            else:
+                return (False, nextframe, quantity)
+        elif(nextframe == 15 and len(rxbuffer) == (quantity + 3 + 7)):
+            # Output values are still being sent, also expecting device reply for function code 15 (0x0F)
+            deviceID = rxbuffer[quantity + 3]
+            address = Combine(rxbuffer[quantity + 4], rxbuffer[quantity + 5])
+            timestamp = datetime.datetime.now()
+            for x in range (0, quantity):
+                print(timestamp, " * Write command", x, ":", rxbuffer[x])
+            return (True, 0, 0)
+
+        elif(nextframe == 16 and len(rxbuffer) == (quantity + 7)):
+            # Exception for function code 16 (0x10)
+            if(rxbuffer[quantity + 3] == '90'):
+                timestamp = datetime.datetime.now()
+                if(rxbuffer[quantity + 4] == '2'):
+                    print(timestamp, " * Illegal Data Address exception")
+                else:
+                    print(timestamp, " * Exception! Code:", rxbuffer[quantity + 4])
+                return (True, 0, 0)
+            else:
+                return (False, nextframe, quantity)
+        elif(nextframe == 16 and len(rxbuffer) == (quantity + 3 + 7)):
+            # Output values are still being sent, also expecting device reply for function code 16 (0x10)
+            deviceID = rxbuffer[quantity + 3]
+            address = Combine(rxbuffer[quantity + 4], rxbuffer[quantity + 5])
+            timestamp = datetime.datetime.now()
+            for x in range (0, quantity):
+                print(timestamp, " * Write command", x, ":", rxbuffer[x])
             return (True, 0, 0)
         else:
             return (False, nextframe, quantity)
--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # PassiveModbusMonitor
-Taps into existing Modbus RTU bus and passively decodes traffic
+Taps into existing Modbus RTU bus and passively decodes traffic.
 
 ## Supported function codes
-* Decode 0x01 Read Coils
-* Decode 0x02 Read Discrete Inputs
-* Decode 0x03 Read Holding Registers
-* Decode 0x04 Read Input Registers
+* 0x01 Read Coils
+* 0x02 Read Discrete Inputs
+* 0x03 Read Holding Registers
+* 0x04 Read Input Registers
+* 0x05 Write Single Coil
+* 0x06 Write Single Register
+* 0x0F Write Multiple Coils
+* 0x10 Write Multiple Registers
 
 ## Other features
 * Automatically decodes a single-precision IEEE 754 float if read quantity for 0x03 is two
@@ -14,25 +18,33 @@ Taps into existing Modbus RTU bus and passively decodes traffic
 * Produal FLTA, wireless gateway for building automation
 * Finder 7E64, single phase energy meter
 * HK Instruments CDT2000, combination CO2/temperature/humidity transmitter
+* proconX modpoll 3.10, Modbus master simulator
 
 ## Example output
 In this example, we are decoding three different measurements from a Finder 7E64 energy meter: mains voltage, instantaneous current, mains frequency and finally instantaneous active power
 
 ```
-2022-01-26 21:12:17.785499 Read holding register for device ID 1 , Start address = 4108 Quantity = 2
-2022-01-26 21:12:17.817414 Response 1 from device ID 1 , data = 17258
-2022-01-26 21:12:17.817414 Response 2 from device ID 1 , data = 12648
-2022-01-26 21:12:17.817414 Float: 234.1929931640625
-2022-01-26 21:12:17.833371 Read holding register for device ID 1 , Start address = 4118 Quantity = 2
-2022-01-26 21:12:17.849834 Response 1 from device ID 1 , data = 16434
-2022-01-26 21:12:17.849834 Response 2 from device ID 1 , data = 45089
-2022-01-26 21:12:17.849834 Float: 2.7920000553131104
-2022-01-26 21:12:17.865790 Read holding register for device ID 1 , Start address = 4152 Quantity = 2
-2022-01-26 21:12:17.897705 Response 1 from device ID 1 , data = 16968
-2022-01-26 21:12:17.897705 Response 2 from device ID 1 , data = 0
-2022-01-26 21:12:17.897705 Float: 50.0
-2022-01-26 21:12:17.913662 Read holding register for device ID 1 , Start address = 4134 Quantity = 2
-2022-01-26 21:12:17.945577 Response 1 from device ID 1 , data = 17420
-2022-01-26 21:12:17.945577 Response 2 from device ID 1 , data = 60457
-2022-01-26 21:12:17.945577 Float: 563.6900024414062
+2022-01-27 22:32:05.833741 Read holding register for device ID 1 , Start address = 4108 Quantity = 2
+2022-01-27 22:32:05.865656  * Response 1 from device ID 1 , data = 17253
+2022-01-27 22:32:05.865656  * Response 2 from device ID 1 , data = 36241
+2022-01-27 22:32:05.865656    * Float: 229.55299377441406
+2022-01-27 22:32:05.881613 Read holding register for device ID 1 , Start address = 4118 Quantity = 2
+2022-01-27 22:32:05.897570  * Response 1 from device ID 1 , data = 16440
+2022-01-27 22:32:05.897570  * Response 2 from device ID 1 , data = 50332
+2022-01-27 22:32:05.897570    * Float: 2.88700008392334
+2022-01-27 22:32:05.913528 Read holding register for device ID 1 , Start address = 4152 Quantity = 2
+2022-01-27 22:32:05.945443  * Response 1 from device ID 1 , data = 16968
+2022-01-27 22:32:05.945443  * Response 2 from device ID 1 , data = 0
+2022-01-27 22:32:05.945443    * Float: 50.0
+2022-01-27 22:32:05.961400 Read holding register for device ID 1 , Start address = 4134 Quantity = 2
+2022-01-27 22:32:05.977357  * Response 1 from device ID 1 , data = 17421
+2022-01-27 22:32:05.977357  * Response 2 from device ID 1 , data = 58950
+2022-01-27 22:32:05.977357    * Float: 567.5980224609375
+2022-01-27 22:32:05.993314 Read holding register for device ID 1 , Start address = 4126 Quantity = 2
+2022-01-27 22:32:06.025229  * Response 1 from device ID 1 , data = 16218
+2022-01-27 22:32:06.025229  * Response 2 from device ID 1 , data = 57672
+2022-01-27 22:32:06.025229    * Float: 0.8550000190734863
 ```
+
+## License
+GPL 3.0
